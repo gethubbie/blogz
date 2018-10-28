a93c853ed2947359618aef7176fe74e447bb6623
Pamela Smith <pamelamsmith7@gmail.com>
2018-10-28 10:31:47 -0500

    Working on Blogz

--- main.py
+++ main.py
@@ -3,20 +3,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:gloryglory@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:wannaHedoit@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'habakkuk2:3proverbs3:1-5psalms119:71'
+app.secret_key = 'jeremiah33:3ephesians3:20isaiah54:17'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(250))
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.Foreignkey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, author):
         self.title = title
         self.body = body
+        self.author = author
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)    
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    blogs = db.relationship('Blog', backref = 'owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup','blog', 'home']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+return redirect('/login')       
 
 @app.route('/')
 def index():
@@ -37,7 +55,6 @@ def newpost():
 
         if blog_title != "" and blog_body != "":
             
-
             new_blog = Blog(blog_title, blog_body)
             db.session.add(new_blog)
             db.session.commit()
